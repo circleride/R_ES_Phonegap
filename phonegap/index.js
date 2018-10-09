c35aa0e54bb6ed19f4079a6a5f23287f7b929a31
maitret <maitret@myhostmx.com>
2018-10-09 12:42:09 -0500

    :)

--- phonegap/index.js
+++ phonegap/index.js
@@ -26,7 +26,7 @@ window.url_server = url_server;
 
 var id_cliente = window.localStorage.getItem("id_cliente");
 if (!id_cliente) {
-window.id_cliente = "app_demo";
+window.id_cliente = "default";
 } else {
 window.id_cliente = id_cliente;
 }
